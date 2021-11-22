bbd8f7e6c776a1417a96cfb92d1f608dfe2e9156
ezequiel <ezelopezdy@gmail.com>
2021-11-22 16:44:15 -0300

    corregido controlador provincia

--- demo/src/main/java/eiv/example/demo/configuracionSeguridad.java
+++ demo/src/main/java/eiv/example/demo/configuracionSeguridad.java
@@ -2,6 +2,7 @@ package eiv.example.demo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,4 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class configuracionSeguridad extends WebSecurityConfigurerAdapter {
 
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+        http.csrf().disable();
+
+    }
+	
+	
 }
--- demo/src/main/java/eiv/example/demo/controladores/ProvinciasControlador.java
+++ demo/src/main/java/eiv/example/demo/controladores/ProvinciasControlador.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import eiv.example.demo.entidades.Provincias;
 import eiv.example.demo.errores.WebException;
@@ -26,7 +28,7 @@ public class ProvinciasControlador {
 	}
 	
 	@PostMapping("/registrar")
-	public Provincias registar(@RequestParam String nombre , String region)throws WebException {
+	public Provincias registar(@RequestParam String nombre ,@RequestParam String region)throws WebException {
 		return servProvincias.registrar(nombre, region);
 	}
 	
